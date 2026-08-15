c71edfede8db7fec9369c0ce17d2fbffc8d1f569
Satsuki884 <62888563+Satsuki884@users.noreply.github.com>
2026-08-15 22:17:34 +0300

    Update WaveInfoText.cs

--- Assets/Game/Scripts/UI/WaveInfoText.cs
+++ Assets/Game/Scripts/UI/WaveInfoText.cs
@@ -6,22 +6,20 @@ namespace MicroJam.Game
     public sealed class WaveInfoText : MonoBehaviour
     {
         [SerializeField] private DayNightCycle dayNightCycle;
-        [SerializeField] private DinosaurSpawner dinosaurSpawner;
         [SerializeField] private TMP_Text textLabel;
 
         private void Awake()
         {
             dayNightCycle ??= FindFirstObjectByType<DayNightCycle>();
-            dinosaurSpawner ??= FindFirstObjectByType<DinosaurSpawner>();
             textLabel ??= GetComponent<TMP_Text>();
         }
 
         private void Update()
         {
-            if (textLabel == null || dayNightCycle == null || dinosaurSpawner == null) return;
+            if (textLabel == null || dayNightCycle == null) return;
             textLabel.text = dayNightCycle.IsDay
-                ? $"День {dayNightCycle.CurrentDayNumber}"
-                : $"Волна {dinosaurSpawner.CurrentWaveNumber}";
+                ? $"Day {dayNightCycle.CurrentDayNumber}"
+                : $"Wave {dayNightCycle.CurrentDayNumber}";
         }
 
     }
